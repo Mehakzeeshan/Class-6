d92540c233d07f53b73ae8fa74cd8ff262714e49
Mehak Noor Mohammad <159150549+Mehakzeeshan@users.noreply.github.com>
2024-12-13 00:20:47 +0500

    first commit

---
+++ app/components/button.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from 'react'
+
+interface IProps {
+    text: string
+}
+
+const Button = (props: IProps) => {
+
+    const handleClick = () => {
+        alert("Button clicked")
+    }
+    
+  return (
+    <button 
+    onClick={handleClick}
+    className='text-white bg-blue-500 px-4 py-2'>
+        {props.text}
+    </button>
+  )
+}
+
+export default Button
